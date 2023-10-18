defeeb7917da2984523f2f3e883df8b763e5f949
Aliniroomand <aliniroomand32@gmail.com>
2023-10-18 18:06:37 +0330

    complete first page without responsive

--- src/components/FirstPage.js
+++ src/components/FirstPage.js
@@ -4,23 +4,25 @@ import styles from './FirstPage.module.css'
 //images
 import adminSVG from "../assets/images/adminSVG.svg";
 import Logo from '../assets/images/mainLoGO (1).png';
+import BG from '../assets/images/desktop BG (1).jpg';
 
 const FirstPage = () => {
     return (
         <div className={styles.container}>
-            <div className={styles.adminpart}>
-                <div>
+            <div className={styles.optionts_part}>
+                <div className={styles.admin_part}>
                 <img src={adminSVG} alt='adminsvg'/>
                 <p>ورود مدیر سایت</p>
                 </div>
-                <div>
+                <div className={styles.enterance_site}>
                 <img src={Logo} alt='Site logo'/>
-                <p>برای ورود به سایت اینجا کلیک کنید</p>
+                <p>برای ورود به سایت<br></br>اینجا کلیک کنید</p>
                 </div>
             </div>
-            <div>
-                <p>برای دیدن جواب آزمایش اینجا کلیک کنید</p>
+            <div className={styles.answers}>
+                <p>برای دیدن جواب آزمایش<br/> اینجا کلیک کنید</p>
             </div>
+         <img src={BG} alt='bg' className={styles.BG_image}/>
         </div>
     );
 };
