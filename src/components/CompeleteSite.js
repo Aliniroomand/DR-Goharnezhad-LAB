78a4a395ae8370af120d4ff94d63b399669f117d
Aliniroomand <aliniroomand32@gmail.com>
2023-11-06 10:22:49 +0330

    make first changes in complete site part

--- src/components/CompeleteSite.js
+++ src/components/CompeleteSite.js
@@ -1,13 +1,93 @@
 import React from 'react';
 import AnimatedPages from './AnimatedPages';
+import { Link } from 'react-router-dom';
+//images
+import DR_Gohar from '../assets/images/DR_goharnezhad_photo.jpg'
+import aboutUs from '../assets/images/aboutUs.svg'
+import microscope from '../assets/images/microscopeBG.png'
+//Style
+import styles from './CompleteSite.module.css'
 
 const CompeleteSite = () => {
     return (
         <AnimatedPages>
-        <div>
+        <>
+            <main>
+        {/* Site title */}
+                <header>
+                    <img src={microscope} />
+                    <h1>
+                        آزمایشگاه تشخیص طبی دکتر گهرنژاد
+                    </h1>
+                </header>
+        {/* Site title_______________ */}
 
-            <h1>complete site</h1>
-        </div>
+        {/* about us */}
+                <article className={styles.aboutUs_container}>
+                        <figure className={styles.aboutUs_DR_title}>
+                            <img src={aboutUs} alt='DRGoharnezhad'/>
+                            <figcaption>
+                                درباره دکتر گهرنژاد
+                            </figcaption>
+                        </figure>
+                    <article className={styles.aboutUs_DR_container}>
+                        <figure>
+                            <img src={DR_Gohar} alt='DRGoharnezhad'/>
+                            <figcaption>
+                                دکتر جمال الدین گهرنژاد
+                                دکترای علوم آزمایشگاهی
+                                ایشان فارغ التحصیل مقطع دکترای علوم آزمایشگاهی از دانشگاه علوم پزشکی ایران در سال ۱۳۷۸ می باشند.
+                                از جمله سوابق اجرایی دکتر گهرنژاد می توان به سمت کارشناس آزمایشگاه میکروبیولوژی رفرانس فارس، کارشناس مسئول آزمایشگاههای مراکز بهداشتی درمانی استان فارس اشاره نمود.
+                                از سال ۹۸ ، ایشان بعنوان مسئول فنی بخش کلینیکال آزمایشگاه پیوند مشغول فعالیت می باشند.
+                            </figcaption>
+                        </figure>
+                    </article> 
+
+                    <article>
+                        <figure>
+                            <img/>
+                            <figcaption>
+                            ،آزمایشگاه تشخیص طبی دکتر گهر نژاد از سال 1399 آغاز به کار کرده و با بهترین و کاملترین تجهیزات روز آزمایشگاهی و با بهره گیری از بهترین پرسنل آزمایشگاهی استان ، در حال خدمت رسانی به مردم می باشد.آزمایشگاه ما بالطف وعنایت پروردگار متعال ودرجهت کمک به تشخیص صحیح ودقیق بیماری ها ,تحقق اهداف پزشکان محترم ,حفظ وارتقاء سطح سلامت جامعه ,کسب اعتبارملی وبه منظور افزایش سطح رضایتمندی بیماران وپزشکان بابهره گیری ازتجهیزات مدرن وبعضامنحصربه فرد آزمایشگاهی دربخش های پاتولوژی و سیتولوژی بارویکردی بیمار محورپابه عرصه خدمت نهاده است .این آزمایشگاه درنظردارد ضمن انجام آزمایشات عمومی واختصاصی درکوتاهترین زمان , نسبت به انجام آزمایشاتی که نیاز به تحقیقات وسیع تری داشته باشند همکاری مستمر داشته وزمینه رابرای ارائه تجارب علمی وعینی شما فراهم نماید. امیداست که به یاری خداوند متعال ودرپی دریافت پیشنهادات وراهنمایی های ارزنده شما بتوانیم درتشخیص به موقع ودقیق بیماری ها خدمات شایسته ای را ارائه نماییم.
+                            </figcaption>
+                        </figure>
+                    </article>
+                </article>
+        {/*about us____________________________________ */}
+
+        {/* our services */}
+                <article>
+                    خدمات ما:انجام کلیه آزمایشهای عمومی و تخصصی اعم از
+                    <ol>
+                        <li>چکاپ کامل (قندخون،چربی و ...)</li>
+                        <li>بیماری های تیروئید،روماتیسم،حساسیت ویتامین ها</li>
+                        <li>کم خونی</li>
+                        <li>انجام آزمایش های هورمونی</li>
+                        <li>دیابت و بیماری های کبدی</li>
+                        <li>آزمایش های کلیوی</li>
+                    </ol>
+                </article>
+        {/* our services _________________________________*/}
+
+        {/* download online results*/}
+                <article>جوابدهی آنلاین
+                    <Link to={'/resultLoginPage'} />
+                </article>
+        {/* download online results_________________*/}
+
+        {/* our adress*/}
+                <article>آدرس ما</article>
+        {/* our adress_________________*/}
+
+        {/* working time*/}
+                <article>ساعت کاری</article>
+        {/* working time*___________________/}
+
+        {/* necessities before exam */}
+                <article>آمادگی های قبل از آزمایش</article>
+        {/* necessities before exam________________ */}
+
+            </main>
+        </>
         </AnimatedPages>
     );
 };
